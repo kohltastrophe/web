2f1db8f172ef2c992799b000aa25589c5d4ce7ff
Kohl <kohltastrophe@gmail.com>
2024-08-28 18:03:01 -0500

    Add faq id links

--- js/script.js
+++ js/script.js
@@ -1,48 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gameCount = document.querySelector('#enum');
-    const visitCount = document.querySelector('#enum2');
-    const backToTop = document.querySelector('.back-to-top');
+document.addEventListener("DOMContentLoaded", () => {
+	if (location.hash !== null && location.hash !== "") {
+		const elementToCollapse = document.querySelector(
+			location.hash + ".collapse",
+		);
 
-    if (backToTop) {
-        window.addEventListener('scroll', () => {
-            const onScreen = isElementOnScreen(backToTop);
-            backToTop.style.opacity = onScreen ? '1' : '0';
-            backToTop.style.transition = 'opacity 0.5s ease-in-out';
-        });
-    }
+		if (elementToCollapse) {
+			elementToCollapse.classList.add("show");
+		}
+	}
 
-    if (gameCount) {
-        animateCount(gameCount, 52, 30, 'lime');
-    }
+	const gameCount = document.querySelector("#enum");
+	const visitCount = document.querySelector("#enum2");
+	const backToTop = document.querySelector(".back-to-top");
 
-    if (visitCount) {
-        animateCount(visitCount, 40, 40, 'lime');
-    }
+	if (backToTop) {
+		window.addEventListener("scroll", () => {
+			const onScreen = isElementOnScreen(backToTop);
+			backToTop.style.opacity = onScreen ? "1" : "0";
+			backToTop.style.transition = "opacity 0.5s ease-in-out";
+		});
+	}
 
-    function isElementOnScreen(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+	if (gameCount) {
+		animateCount(gameCount, 52, 30, "lime");
+	}
 
-    function animateCount(element, maxValue, interval, color) {
-        let number = 0;
-        const intervalId = setInterval(() => {
-            element.textContent = number;
-            if (number === maxValue) {
-                const parent = element.parentElement;
-                const icon = parent.querySelector('i');
-                icon.style.color = color;
-                icon.style.transition = 'color 0.5s ease-in-out';
-            }
-            number++;
-            if (number > maxValue) {
-                clearInterval(intervalId);
-            }
-        }, interval);
-    }
+	if (visitCount) {
+		animateCount(visitCount, 40, 40, "lime");
+	}
+
+	function isElementOnScreen(element) {
+		const rect = element.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+				(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+
+	function animateCount(element, maxValue, interval, color) {
+		let number = 0;
+		const intervalId = setInterval(() => {
+			element.textContent = number;
+			if (number === maxValue) {
+				const parent = element.parentElement;
+				const icon = parent.querySelector("i");
+				icon.style.color = color;
+				icon.style.transition = "color 0.5s ease-in-out";
+			}
+			number++;
+			if (number > maxValue) {
+				clearInterval(intervalId);
+			}
+		}, interval);
+	}
 });
